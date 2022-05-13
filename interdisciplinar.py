b58c63026ab05fca0c15bba2d74a8a4c5479f410
André <67207390+andrefrez02@users.noreply.github.com>
2022-05-13 20:16:56 -0300

    Update interdisciplinar.py

--- interdisciplinar.py
+++ interdisciplinar.py
@@ -1,8 +1,21 @@
-op = 'valorPadrao0'
+op = 'x'
 
-def converter(dec = int):
-    val = ''
-    decimal = dec
+decimal = int(input('Entrada:\nDigite um número Inteiro e em base Decimal: '))
+if decimal > 0:
+    op = int(input(f'Para qual base você deseja converter o número {decimal}?' + 
+                    '\n\b[1 - Binário; 2 - Octadecimal ou 3 - Hexadecimal]: '))
+
+print('\nSaída:')
+if op == 1: divisor = 2
+elif op == 2: divisor = 8
+elif op == 3: divisor = 16
+else: 
+    divisor = 0
+    print('Valor inválido!\nO valor digitado deve estar dentro de [1, 2 e 3]!')
+
+resultado = ''
+dec = decimal
+if not divisor == 0:
     while dec != 0:
         r = dec % divisor
         if divisor == 16:
@@ -13,27 +26,6 @@ def converter(dec = int):
                 case 13: r = 'D'
                 case 14: r = 'E'
                 case 15: r = 'F'
-        val = str(r) + val
+        resultado = str(r) + resultado
         dec = dec // divisor
-    print(f'{decimal}¹\u2070 convertido para base {divisor} é: {val}')
-    
-decimal = int(input('Entrada:\nDigite um número Inteiro e Decimal: '))
-if decimal > 0:
-    op = int(input(f'Para qual base você deseja converter o número {decimal}?' + 
-                    '\n\b[1 - Binário; 2 - Octadecimal ou 3 - Hexadecimal]: '))
-    
-print('\nSaída:')
-match op:
-    case 'valorPadrao0':
-        print('Valor inválido!\nO valor digitado não pode ser menor ou igual a zero!')
-    case 1:
-        divisor = 2
-        converter(decimal)
-    case 2:
-        divisor = 8
-        converter(decimal)
-    case 3:
-        divisor = 16
-        converter(decimal)
-    case _:
-        print('Valor inválido!\nO valor digitado deve estar dentro de [1, 2 e 3]!')
+    print(f'{decimal}¹\u2070 convertido para base {divisor} é: {resultado}')
